a9a43052204d1fd38f4170cb05ceea3a52d021fa
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2025-09-05 15:16:41 +0200

    refactor(projet) restructure app with App component and simplified main entry point

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import useTheme from './hooks/useTheme';
+import lightTheme from './themes/lightTheme';
+import darkTheme from './themes/darkTheme';
+
+import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
+import Footer from './components/Footer.jsx';
+
+export default function App() {
+  const { theme, toggleTheme } = useTheme(lightTheme, darkTheme, 'light');
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Main />
+      <Footer onToggleTheme={toggleTheme} />
+    </ThemeProvider>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,10 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import Main from './components/Main.jsx';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <Main />
-    <Footer />
+    <App />
   </StrictMode>
 );
